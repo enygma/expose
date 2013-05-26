d00f05c70a72451ed5a641359da2e3e69339fae8
enygma <enygma@phpdeveloper.org>
2013-05-26 07:20:59 -0500

    updating setting exceptions to allow for setting of arrays

--- src/Expose/Manager.php
+++ src/Expose/Manager.php
@@ -182,7 +182,8 @@ class Manager
      */
     public function setException($path)
     {
-        $this->exceptions[] = $path;
+        $path = (!is_array($apth)) ? array($path) : $path;
+        $this->exceptions[] = array_merge($this->exceptions, $path);
     }
 
     /**
