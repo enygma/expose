c6fa68ba42a7f21891bbaad64fd95806c3fb3029
enygma <enygma@phpdeveloper.org>
2013-06-01 13:54:53 -0500

    adding lots of docblocks and the getRule to the Filter class

--- src/Expose/Filter.php
+++ src/Expose/Filter.php
@@ -4,12 +4,41 @@ namespace Expose;
 
 class Filter
 {
+    /**
+     * Filter ID
+     * @var integer
+     */
     private $id = null;
+
+    /**
+     * Filter Regex match rule
+     * @var string
+     */
     private $rule = null;
+
+    /**
+     * Filter description
+     * @var string
+     */
     private $description = null;
+
+    /**
+     * Filter tag set
+     * @var array
+     */
     private $tags = array();
+
+    /**
+     * Filter impact rating
+     * @var integer
+     */
     private $impact = 0;
 
+    /**
+     * Init the filter and set the data if given
+     * 
+     * @param array $data Filter data [optional]
+     */
     public function __construct(array $data = null)
     {
         if ($data !== null) {
@@ -17,6 +46,11 @@ class Filter
         }
     }
 
+    /**
+     * Load the data into the filter object
+     * 
+     * @param array $data Filter data
+     */
     public function load($data)
     {
         if (is_object($data)) {
@@ -31,14 +65,48 @@ class Filter
         }
     }
 
+    /**
+     * Get the current filter's impact
+     * 
+     * @return integer Impact level
+     */
     public function getImpact()
     {
         return $this->impact;
     }
 
+    /**
+     * Set the impact level of the filter
+     * 
+     * @param integer $impact Impact rating (Ex. "8" or "3")
+     * @return \Expose\Filter instance
+     */
+    public function setImpact($impact)
+    {
+        $this->impact = $impact;
+        return $this;
+    }
+
+    /**
+     * Get the current list of tags for the filter
+     * 
+     * @return array Set of tags
+     */
     public function getTags()
     {
         return $this->tags->tag;
+    }
+
+    /**
+     * Set the tags that match on the filter
+     * 
+     * @param array $tags Tag set
+     * @return \Expose\Filter instance
+     */
+    public function setTags($tags)
+    {
+        $this->tags->tag = $tags;
+        return $this;
     }
 
     public function getDescription()
@@ -46,9 +114,60 @@ class Filter
         return $this->description;
     }
 
+    /**
+     * Set the current Filter's description
+     * 
+     * @param string $desc Description of filter
+     * @return \Expose\Filter instance
+     */
+    public function setDescription($desc)
+    {
+        $this->description = $desc;
+        return $this;
+    }
+
+    /**
+     * Get the current filter's ID
+     * 
+     * @return integer Filter ID
+     */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set the current filter's ID
+     * 
+     * @param integer $id Filter ID
+     * @return \Expose\Filter instance
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Get the current Rule value
+     * 
+     * @return string Regex rule
+     */
+    public function getRule()
+    {
+        return $this->rule;
+    }
+
+    /**
+     * Set the current regex match rule
+     * 
+     * @param string $rule Regex match string
+     * @return \Expose\Filter instance
+     */
+    public function setRule($rule)
+    {
+        $this->rule = $rule;
+        return $this;
     }
 
     public function execute($data)
