b669eb9e0a89828b088ec79d150508d783d4cdb9
enygma <enygma@phpdeveloper.org>
2013-06-02 06:53:46 -0500

    adding more Filter class tests

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -11,6 +11,12 @@ class FilterTest extends \PHPUnit_Framework_TestCase
         $this->filter = new \Expose\Filter();
     }
 
+    /**
+     * Test the getter/setter for impact value
+     * 
+     * @covers \Expose\Filter::setImpact
+     * @covers \Expose\Filter::getImpact
+     */
     public function testGetSetImpact()
     {
         $impact = 10;
@@ -21,6 +27,12 @@ class FilterTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * Test the getter/setter for the tag value
+     * 
+     * @covers \Expose\Filter::setTags
+     * @covers \Expose\Filter::getTags
+     */
     public function testGetSetTags()
     {
         $tags = array('foo' => 'bar');
@@ -31,6 +43,12 @@ class FilterTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * Test the getter/setter for the description
+     * 
+     * @covers \Expose\Filter::setDescription
+     * @covers \Expose\Filter::getDescription
+     */
     public function testGetSetDescription()
     {
         $desc = 'this is a description';
@@ -38,6 +56,95 @@ class FilterTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             $this->filter->getDescription(),
             $desc
+        );
+    }
+
+    /**
+     * Test the getter/setter for the ID
+     * 
+     * @covers \Expose\Filter::setId
+     * @covers \Expose\Filter::getId
+     */
+    public function testGetSetId()
+    {
+        $id = 12;
+        $this->filter->setId($id);
+        $this->assertEquals(
+            $this->filter->getId(),
+            $id
+        );
+    }
+
+    /**
+     * Test the gettet/setter for the regex rule
+     * 
+     * @covers \Expose\Filter::setRule
+     * @covers \Expose\Filter::getRule
+     */
+    public function testGetSetRule()
+    {
+        $rule = '^foo[0-9]+';
+        $this->filter->setRule($rule);
+        $this->assertEquals(
+            $this->filter->getRule(),
+            $rule
+        );
+    }
+
+    /**
+     * Test that the result is true when a match is found
+     *     for the rule
+     * 
+     * @covers \Expose\Filter::setRule
+     * @covers \Expose\Filter::execute
+     */
+    public function testMatchDataValid()
+    {
+        $data = 'foo1234';
+        $rule = '^foo[0-9]+';
+
+        $this->filter->setRule($rule);
+        $this->assertTrue(
+            $this->filter->execute($data)
+        );
+    }
+
+    /**
+     * Test that the result is false when a match is not found
+     * 
+     * @covers \Expose\Filter::setRule
+     * @covers \Expose\Filter::execute
+     */
+    public function testMAtchDataInvalid()
+    {
+        $data = 'barbaz';
+        $rule = '^foo[0-9]+';
+
+        $this->filter->setRule($rule);
+        $this->assertFalse(
+            $this->filter->execute($data)
+        );
+    }
+
+    /**
+     * Test that the output of the data in an array is correct
+     * 
+     * @covers \Expose\Filter::toArray
+     */
+    public function testOutputAsArray()
+    {
+        $data = array(
+            'id' => 1234,
+            'rule' => '^foo[0-9]+',
+            'description' => 'this is a test',
+            'tags' => array('csrf', 'xss'),
+            'impact' => 3
+        );
+        $filter = new \Expose\Filter($data);
+        $data['tags'] = 'csrf, xss';
+
+        $this->assertEquals(
+            $filter->toArray(), $data
         );
     }
 }
