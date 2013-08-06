596865b42850147d1f54d32cf33667d8836fd3f0
enygma <enygma@phpdeveloper.org>
2013-08-05 21:14:45 -0500

    Adding PSR-3 methods to the MockLogger

--- tests/MockLogger.php
+++ tests/MockLogger.php
@@ -2,10 +2,54 @@
 
 namespace Expose;
 
-class MockLogger
+class MockLogger extends \Expose\Log
 {
     public function __call($func, $args)
     {
         return true;
     }
+
+    public function emergency($message, array $context = array())
+    {
+    	return true;
+    }
+	public function alert($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function critical($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function error($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function warning($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function notice($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function info($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function debug($message, array $context = array())
+	{
+		return true;
+	}
+
+	public function log($level, $message, array $context = array())
+	{
+		return true;
+	}
 }
