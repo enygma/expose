b428c20968f30599b01a15b39e70d766e1b61df1
Chris Cornutt <ccornutt@ccornutt.local>
2013-11-05 08:21:24 -0600

    removing unused "use" statements

--- src/Expose/Console/Command/FilterCommand.php
+++ src/Expose/Console/Command/FilterCommand.php
@@ -4,7 +4,6 @@ namespace Expose\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
 class FilterCommand extends Command
@@ -24,7 +23,7 @@ class FilterCommand extends Command
 
     /**
      * Execute the filter command
-     * 
+     *
      * @param  InputInterface  $input  Input object
      * @param  OutputInterface $output Output object
      * @return null
--- src/Expose/Console/Command/ProcessQueueCommand.php
+++ src/Expose/Console/Command/ProcessQueueCommand.php
@@ -4,7 +4,6 @@ namespace Expose\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
 class ProcessQueueCommand extends Command
