8cd55b28f69fc77401e613eac0894a609c448de0
Jorge Hoya <jorge@nosoynadie.net>
2015-05-18 22:04:58 +0200

    First version of folder "example"

---
+++ example/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+}
+spl_autoload_register('autoload');
